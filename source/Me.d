00818ad33546dce4398f2cbe58c8efe817014e60
liuweiming <351677994@qq.com>
2015-06-10 17:49:09 +0800

    fir commit

--- source/Me.d
+++
@@ -1,5 +0,0 @@
-../source//Me.o: ../source//Me.cpp ../source//stdafx.h ../source//Me.h \
- ../source//MsgGenerator.h ../source//PokerPlayer.h ../source//Pokers.h \
- ../source//Message.h ../source//ClientSocket.h ../source//Socket.h \
- ../source//Players.h ../source//circleList.h ../source//Probability.h \
- ../source//SeatPoker.h ../source//Strategy.h
